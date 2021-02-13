bec9e4afe6c40d2b1923711396e5d671d69a61ad
MosqueraSt3 <esmosquera66@misena.edu.co>
2021-02-13 09:35:08 -0500

    sirviendo estaticos

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
 
+// CONTENIDO ESTATICO
+app.use( express.static('public') )
+
+// RUTAS
 app.get('/', (req,res) => {
     res.send('hola mundo')
 })
 
+app.get('*', (req,res) => {
+    res.sendFile(__dirname + '/public/404.html')
+})
+
+// SERVIDOR
 app.listen(3000, () => console.log('||ON|| 3000'))
