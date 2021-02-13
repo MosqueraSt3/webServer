af5e48a13f720fcced9e19e627cd5d0442a1f760
MosqueraSt3 <esmosquera66@misena.edu.co>
2021-02-13 09:22:18 -0500

    express

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
-http.createServer( (req,res) => {
-    res.writeHead(200, { 'Content-Type': 'application/json' })
-    const persona = {
-        id: 1,
-        nombre: 'Steven'
-    }
-    res.write(persona)
-    res.end()
-}).listen( 3000 )
+app.get('/', (req,res) => {
+    res.send('hola mundo')
+})
 
-console.log('||ON|| 3000')
+app.listen(3000, () => console.log('||ON|| 3000'))
